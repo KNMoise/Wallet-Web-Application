758926cc40895104c07a85f669d105b09af62525
Moise NIZEYIMANA <obiomoise@gmail.com>
2025-01-16 17:10:33 +0200

    Fix : error in ui components now stop using alias to relative path file

--- wallet/vite.config.js
+++ wallet/vite.config.js
@@ -1,7 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(__dirname, './src/components'),
+      // '@/utils': path.resolve(__dirname, './src/utils'),
+      '@/styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
 })
