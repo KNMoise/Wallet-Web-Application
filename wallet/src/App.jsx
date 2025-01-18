e35ce5a97e71aaf5ab43a072e19bcdced9ea6e82
Moise NIZEYIMANA <obiomoise@gmail.com>
2025-01-18 15:43:42 +0200

    atleast i learned something

--- wallet/src/App.jsx
+++ wallet/src/App.jsx
@@ -1,25 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from "./pages/auth/LoginPage";
-import RegisterPage from "./pages/auth/RegisterPage";
-import DashboardLayout from "./components/layouts/DashboardLayout";
-import DashboardPage from './pages/dashboard/DashboardPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Auth routes */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        
-        {/* Dashboard routes */}
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<Navigate to="/dashboard" replace />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-        </Route>
-        
-        {/* Fallback route */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
